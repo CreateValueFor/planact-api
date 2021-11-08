416af2ef741a6e7ae12c30fd18cd94c711b1ba99
Lee Min Gi <2019143077@yonsei.ac.kr>
2021-11-08 20:29:01 +0900

    Add plan RESTFUL api #7

--- routes/plan.js
+++ routes/plan.js
@@ -2,8 +2,27 @@ const express = require("express");
 const User = require("../models/user");
 const UserPlans = require("../models/userPlans");
 const fs = require("fs");
-const router = express.Router();
+const multer = require("multer");
+const PlanSummary = require("../models/plan");
+const { v4: uuidv4 } = require("uuid");
 
+let imageID = uuidv4();
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "uploads/");
+  },
+  filename: (req, file, callback) => {
+    const ext = file.originalname.split(".")[1];
+    callback(null, `${imageID}.${ext}`);
+  },
+  encoding: (req, file, callback) => {
+    callback(null, "utf-8");
+  },
+});
+const uploader = multer({ storage: storage });
+const router = express.Router();
+//플랜 불러오기 by IP
 router.get("/", async (req, res, next) => {
   const category = req.query["category"];
   try {
@@ -33,7 +52,7 @@ router.get("/", async (req, res, next) => {
     return next(error);
   }
 });
-
+//Plan 불러오기 by post
 router.post("/load", async (req, res, next) => {
   const { category } = req.body;
   try {
@@ -101,6 +120,120 @@ router.post("/save", async (req, res, next) => {
   } catch (error) {
     console.error(error);
     return next(error);
+  }
+});
+
+//plan 업로드
+router.post("/summary", uploader.single("thumb"), async (req, res, next) => {
+  let ext;
+  if (req.file) {
+    ext = req.file.originalname.split(".")[1];
+  } else {
+    imageID = "";
+    ext = "";
+  }
+  const { title, sns, author, category, email } = req.body;
+
+  const planID = uuidv4();
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (!exUser) {
+      return res.status(202).json({
+        code: 202,
+        message: "유저가 존재하지 않습니다.",
+      });
+    }
+    await PlanSummary.create({
+      author,
+      title,
+      sns,
+      category,
+      planID,
+      imgID: `${imageID}.${ext}`,
+      UserId: exUser.id,
+    });
+    return res.json({
+      code: 200,
+      req: req.body,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
+router.get("/summary", async (req, res, next) => {
+  const email = req.query.email;
+  const id = req.query.id;
+  try {
+    if (email) {
+      const exUser = await User.findOne({ where: { email } });
+
+      const plans = await PlanSummary.findAll({
+        where: { UserId: exUser["id"] },
+      });
+      // console.log(plans);
+      return res.status(200).json({
+        code: 200,
+        plans,
+      });
+    }
+    if (id) {
+      const plans = await PlanSummary.findOne({
+        where: { id },
+      });
+      return res.status(200).json({
+        code: 200,
+        plans,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.post("/daily", async (req, res, next) => {
+  let exJSON;
+  try {
+    const planId = req.body.planId;
+    const path = `public/plans/${planId}.json`;
+    let file = req.body.dailyplan;
+
+    if (fs.existsSync(path)) {
+      exJSON = JSON.parse(fs.readFileSync(path));
+      exJSON.push(file);
+    } else {
+      exJSON = [];
+      exJSON.push(file);
+    }
+    file = JSON.stringify(exJSON);
+    fs.writeFileSync(path, file);
+    return res.json({
+      code: 200,
+      message: "정상적으로 저장되었습니다.",
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get("/daily", async (req, res, next) => {
+  const id = req.query.id;
+  const path = `public/plans/${id}.json`;
+  try {
+    if (!fs.existsSync(path)) {
+      res.json({
+        code: 202,
+        message: "파일이 존재하지 않습니다.",
+      });
+    }
+    const plans = JSON.parse(fs.readFileSync(path));
+    res.json({
+      code: 200,
+      plans,
+    });
+  } catch (error) {
+    console.error(error);
   }
 });
 
